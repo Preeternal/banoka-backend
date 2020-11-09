9961c8bbc9143eaff76acd3915d19f8afdc669b4
Preeternal <preeternal@ya.ru>
2020-11-09 15:52:29 +0200

    starting with currency entity

---
+++ src/entities/Currency.ts
@@ -0,0 +1,43 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class Currency extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @Column({ unique: true })
+  name!: string;
+
+  @Field()
+  @Column({ unique: true })
+  nameEng: string;
+
+  @Field()
+  @Column({
+    unique: true,
+    // nullable: true,
+  })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
